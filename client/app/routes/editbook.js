6998ee080111130ba02599ebfc784de2df2311d4
jose andradez <xjgamx@gmail.com>
2015-06-04 16:03:33 -0430

    fixing editbook and sideloading

--- client/app/routes/editbook.js
+++ client/app/routes/editbook.js
@@ -1,32 +1,36 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	
 		model: function  (params) {
 		return this.store.find('book',params.book_id);//este no es necesario, ember lo asume
 	},
 	setupController: function  (controller,model) {
-		controller.set('model',model)
+		this._super(controller,model);
+		//hcontroller.set('model',model)
+		// controller.set('selectedowner',model.get('owner'))
 		controller.set('users', this.store.find('user'));
+		this.controllerFor(this.routeName).set('selectedowner',model.get('owner'));
 	},
 	   actions:{
 		save: function (){
-			var _this = this
+			var _this = this;
 			 //var m = this.get('model');
 			 var m = this.currentModel;
 			// var m = this.modelFor('newbook')
 			 //console.log(m.get('owner').get('name'))
-			 var selected = this.controllerFor(this.routeName).get('selectedowner')
+			 //var selected = this.controllerFor(this.routeName).get('selectedowner')
 
 
 
 
-			 console.log(selected)
-			 m.set('owner',selected)
-			 console.log(m.get('name'))
+			 //console.log(selected)
+			 //m.set('owner',selected)
+			 console.log(m.get('name'));
 			  m.save()
 			 .then(function  () {
-			 	_this.transitionTo('books')
-			 })
+			 	_this.transitionTo('books');
+			 });
 		}
 	
    }
